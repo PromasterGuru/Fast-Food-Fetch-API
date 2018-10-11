04f6ee3498485872476d7bb2f7b6efe054710eed
root <root@localhost.localdomain>
2018-10-11 13:32:20 +0300

    Refactor login

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -33,3 +33,37 @@ signUp = () => {
   //catch any error that might occur during registration
   .catch(error => console.log(error));
 }
+
+//User login
+login = () => {
+  var proxyUrl = 'https://morning-springs-84037.herokuapp.com/'
+  let data = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value
+  };
+  username = data.username
+  const url = 'https://pro-fast-food-fast-api.herokuapp.com/api/v2/auth/login';
+  // submit registration data data to the server
+  fetch(proxyUrl  + url, {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(function(response){
+    return response.json();
+  }).then(function(data){
+    let msg = data.Message;
+    if(msg === "Login successful, Welcome " + username){
+      document.getElementById('error').innerHTML = msg;
+      setTimeout(() => {document.getElementById("error").innerHTML = "";}, 4000);
+      window.location.href = "/menu";
+    }
+    else{
+      document.getElementById('error').innerHTML = msg;
+      setTimeout(() => {document.getElementById("error").innerHTML = "";}, 5000);
+    }
+  })
+  //catch any error that might occur during registration
+  .catch(error => console.log(error));
+}
