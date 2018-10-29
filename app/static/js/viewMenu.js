9883e2e6559c56922802d652f51e9753c7fc8a8e
Promaster <promaster@localhost.localdomain>
2018-10-29 11:07:22 +0300

    Add logo to login and signup page

--- app/static/js/viewMenu.js
+++ app/static/js/viewMenu.js
@@ -1,6 +1,7 @@
 const access_token = localStorage.getItem("token");
 const proxyUrl = "https://morning-springs-84037.herokuapp.com/";
 let orders;
+let count;
 /**Menu Url*/
 const url = "https://pro-fast-food-fast-api.herokuapp.com/api/v2/menu";
 fetch(proxyUrl + url)
@@ -34,10 +35,78 @@ fetch(proxyUrl + url)
       </span>
       </div>`;
   }
+  document.getElementById("food_items").innerHTML += `
+  <span id="pagination">
+  <button onclick="getNext();">Next</button>
+  <button onclick="getPrevious();">Previous</button>
+  <button onclick="getLast();">Last</button>
+  <button onclick="getFirst();">First</button>
+  <span>
+  `
 })
 .catch(error => console.log(error));
 
+/**Add selected food item to the local storage */
 setItem = (id) => {
   localStorage.setItem("item", orders[id-1].meal_name);
   window.location.href=`/specific-order/${id}`;
 }
+
+/**Pure JavaScript page pagination*/
+let curPage = 1;
+let lastPage = orders.length;
+let nextPage = curPage + 1;
+let firstPage = 0;
+let prevPage = curPage -1;
+
+/**Get page */
+getPage = (page) => {
+  let result;
+  if(page === "previous"){
+    result = previousPage;
+  }
+  else if(page = "next"){
+    result = nextPage;
+  }
+  else if(page = "first"){
+    result = firstPage;
+  }
+  else{
+    result = lastPage;
+  }
+  return result;
+}
+
+/**Get previous page*/
+getPrevious = () => {
+  if(count > 0){
+    getPage("previous");
+  }
+  alert(getPage("previous"));
+}
+
+/**Get next page*/
+getNext = () => {
+  if(count < lastPage){
+    getPage("next");
+  }
+  alert(getPage("next"));
+}
+
+/**Get first page*/
+getFirst = () => {
+  if(count > 0){
+    getPage("first");
+  }
+  alert(getPage("first"));
+}
+
+/**Get last page*/
+getLast = () => {
+  if(count < lastPage){
+    getPage("last");
+  }
+  alert(getPage("last"));
+}
+
+
